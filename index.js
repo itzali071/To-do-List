da4261e69703f9bc80ebd7d82271c06c0f7a2dea
Muhammad Ali Raza <155883823+itzali071@users.noreply.github.com>
2024-05-18 04:04:21 -0700

    To do

---
+++ index.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+let todos_list = [];
+let condition = true;
+while (condition) {
+    let addTask = await inquirer.prompt([
+        {
+            type: "input",
+            name: "todo",
+            message: "What do you want to do?",
+        },
+        //27041969
+        {
+            type: "confirm",
+            name: "todo_more",
+            message: "are you sure you want to add this ",
+            default: "false",
+        },
+    ]);
+    todos_list.push(addTask.todo);
+    condition = addTask.todo_more;
+    console.log(todos_list);
+}
